74393d7c1892b8231d51223e5b285860cd6300bd
Xingchen Liang <liang@maichong.it>
2022-11-07 19:41:48 +0800

    1.0.1

--- src/node.ts
+++ src/node.ts
@@ -4,4 +4,4 @@ import * as fetch from 'node-fetch';
 import * as FormData from 'form-data';
 import inject from './inject';
 
-export default inject(fetch, FormData, 'Akita/1.0.0 (+https://github.com/liangxingchen/akita)');
+export default inject(fetch, FormData, 'Akita/1.0.1 (+https://github.com/liangxingchen/akita)');
